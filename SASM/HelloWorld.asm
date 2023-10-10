6c10bb1bd4a023cbcc01548137fa28fccb3a7eb9
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-10-10 11:25:56 +0900

    :sparkles: ㅣ byte

--- SASM/HelloWorld.asm
+++ SASM/HelloWorld.asm
@@ -3,27 +3,17 @@
 section .text
 global CMAIN
 main:
-    ;write your code here
     
-    ; 메모를 남길 수 있는 기능 = 주석
+    ; 8 bit = 1 Byte
+    ; 16 bit = 2 Byte = 1 word
+    ; 32 bit = 4 byte = 2 word = 1 dword(double-word)
+    ; 64 bit = 8 byte = 4word = 1 qword (quad-word)
     
-    ; 10진수 (0 1 2 3 4 5 6 7 8 9)
-    ; 10 11 12 13 ... 19 20 
-    
-    ; 2진수(0 1)
-    ; 0 1 10 
-    ; 0b0 0b1 0b10 0b11 0b100
-    
-    ; 16진수(0 1 2 3 4 5 6 7 8 9 A B C D E F)
-    ; 0 1 2 3 4 5 6 7 8 9 A B C D E F 10
-    ; 0x00 
-    
-    ; 0b10010101 = 0x95
+
     
     PRINT_STRING msg
     
     xor rax, rax
     ret
     
-section .data
-    msg db 'Hello World', 0x00
+;section .data
