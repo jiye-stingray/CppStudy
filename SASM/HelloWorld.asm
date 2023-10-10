2cdfe4642d03c717e83d3dab8a39412349842185
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-10-10 11:54:24 +0900

    🤩l 레지스터 기초

--- SASM/HelloWorld.asm
+++ SASM/HelloWorld.asm
@@ -3,15 +3,19 @@
 section .text
 global CMAIN
 main:
+    mov rbp, rsp; for correct debugging
     
     ; 8 bit = 1 Byte
     ; 16 bit = 2 Byte = 1 word
     ; 32 bit = 4 byte = 2 word = 1 dword(double-word)
     ; 64 bit = 8 byte = 4word = 1 qword (quad-word)
     
-
+    mov eax, 0x1234
+    mov rbx, 0x12345678
+    mov cl, 0xff
     
-    PRINT_STRING msg
+    mov al, 0x00 
+    mov rax, rdx
     
     xor rax, rax
     ret
