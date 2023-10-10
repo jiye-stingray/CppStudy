7c5cac00c6d762892178dffa93c07e31de8e1d73
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-10-10 11:02:24 +0900

    :sparkles: ㅣ 진법 기초

--- SASM/HelloWorld.asm
+++ SASM/HelloWorld.asm
@@ -5,6 +5,21 @@ global CMAIN
 main:
     ;write your code here
     
+    ; 메모를 남길 수 있는 기능 = 주석
+    
+    ; 10진수 (0 1 2 3 4 5 6 7 8 9)
+    ; 10 11 12 13 ... 19 20 
+    
+    ; 2진수(0 1)
+    ; 0 1 10 
+    ; 0b0 0b1 0b10 0b11 0b100
+    
+    ; 16진수(0 1 2 3 4 5 6 7 8 9 A B C D E F)
+    ; 0 1 2 3 4 5 6 7 8 9 A B C D E F 10
+    ; 0x00 
+    
+    ; 0b10010101 = 0x95
+    
     PRINT_STRING msg
     
     xor rax, rax
