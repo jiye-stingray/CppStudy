4953984f0612e076a642ea06add6b4fe86970d03
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-10-11 18:02:27 +0900

    :sparkles: ㅣ CMP (IF)

--- SASM/HelloWorld.asm
+++ SASM/HelloWorld.asm
@@ -5,64 +5,48 @@ global CMAIN
 main:
     mov rbp, rsp; for correct debugging
     
-    ; 쉬프트(Shift) 연산, 논리(logical) 연산
+    ; 분기문 (if)
+    ; 특정 조건에 따라서 코드 흐름을 제어하는 것 
+    ; ex) 스킬 버튼 눌렀는가?YES -> 스킬 사용
+    ; ex) 제한 시간 내에 던전 입장 수락 버튼을 눌렀는가? YES -> 입장, NO -> 던전 취소
     
-    mov eax, 0x12345678
-    PRINT_HEX 4, eax
+    ; 조건 -> 흐름
+    
+    ; CMP dst, src (dst가 기준)
+    ; 비교를 한 결과물은 Flag Register 저장
+    
+    ; JMP [lable] 시리즈
+    ; JMP : 무조건 jump
+    ; JE : JumpEquals 같으면 jump
+    ; JNE : JumpNotEquals 다르면 jump
+    ; JG : JumpGreater 크면 jump
+    ; JGE : JumpGreaterEquals 크거나 같으면 jump
+    ; JL 
+    ; JLE 
+    
+    ; 두 숫자가 같으면 1, 아니면 0을 출력하는 프로그램
+    
+    mov rax, 10
+    mov rbx, 10
+    
+    cmp rax, rbx
+    
+    je LABEL_EQUAL  
+    
+    ; je에 의해 점프를 안했다면, 같지 않다는 의미
+    mov rcx, 0
+    jmp LABEL_EQUAL_END
+
+LABEL_EQUAL: 
+    mov rcx, 1
+LABEL_EQUAL_END:
+
+    PRINT_HEX 1, rcx
     NEWLINE
-    shl eax, 8
-    PRINT_HEX 4, eax
-    NEWLINE
-    shr eax, 8
-    PRINT_HEX 4, eax
-    NEWLINE
     
-    ; 곱셈/나눗셈
-    ; 게임서버에서 ObjectID를 만들어줄 때
     
-    ; not and or xor
     
-    ; 조건A : 잘생겼다
-    ; 조건B : 키가 크다
     
-    ; not A : 잘생겼다의 반대 -> (0이면 1, 1이면 0)
-    ; A and B : 잘생겼고 and 키도 크고 -> 둘다 1이면 1, 아니면 0
-    ; A or B : 잘생겼거나 or 키가 크거나 -> 둘중 하나라도 1이면 1, 아니면 0
-    ; A xor B : 둘다 1이거나 둘다 0이면 0, 아니면 1
-    
-    mov al, 0b10010101
-    mov bl, 0b01111100
-   
-    ; 0b0001 0100 = 0x14
-    and al, bl ; al = al and bl
-    PRINT_HEX 1, al
-    NEWLINE
-   
-    ; 0b1110 1011 = 0xeb
-    not al 
-    PRINT_HEX 1, al
-    NEWLINE
-    
-    ; 응용 사례 : bitflag
-    
-    mov al, 0b10010101
-    mov bl, 0b01111100
-    
-    NEWLINE
-    PRINT_HEX 1, al
-    NEWLINE
-    xor al, bl
-    PRINT_HEX 1, al
-    NEWLINE
-    xor al, bl
-    PRINT_HEX 1, al
-    NEWLINE
-    ; 동일한 값으로 xor 두번하면 자기 자신으로 되돌아오는 특성
-    ; 암호학에서 유용하다! (value xor key)
-    
-    ; mov al, 0
-    xor al, al
-    PRINT_HEX 1, al
     
     xor rax, rax
     ret
