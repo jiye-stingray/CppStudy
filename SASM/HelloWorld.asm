69f5eea1303ac3dbd8fa02464adec3e5053194be
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-10-12 20:07:15 +0900

    :sparkles: ㅣ 두 값 비교 함수

--- SASM/HelloWorld.asm
+++ SASM/HelloWorld.asm
@@ -4,43 +4,42 @@ section .text
 global CMAIN
 main:
     mov rbp, rsp; for correct debugging
-    mov rbp, rsp; for correct debugging
+
+    ; 함수 (프로시저 procedure 서브루틴 subtroutine)
     
-    ; 배열과 주소
+    ;call PRINT_MSG
     
-    ; 배열 : 동일한 타입의 데이터 묶음 
-    ; - 배열을 구성하는 각 값을 배열 요소(element)라고 함
-    ; - 배열의 위치를 가리키는 숫자를 인덱스(index)라고 함
-    
-    
-    ; 주소
-    ; [시작 주소 + 인덱스 * 크기]
-    
-    mov rax, a
-    
-    ; 연습 문제 : a 배열의 모든 데이터 출력해보기
-    
-    xor ecx, ecx
-LABEL_PRINT_A:
-    ;PRINT_HEX 1, [a + ecx]
-    ;NEWLINE 
-    inc ecx
-    cmp ecx, 5
-    jne LABEL_PRINT_A
-    
-    
-    xor ecx, ecx
-LABEL_PRINT_B:
-    PRINT_HEX 2, [b+ecx*2]
+    mov eax, 10
+    mov ebx, 15
+    call MAX
+    PRINT_DEC 4, ecx
     NEWLINE
-    inc ecx
-    cmp ecx, 5
-    jne LABEL_PRINT_B
-    
     
     xor rax, rax
     ret
     
+    
+PRINT_MSG:
+    PRINT_STRING msg
+    NEWLINE
+    ret 
+
+; ex) 두 값중 더 큰 값을 반환하는 max
+; 근데 2개의 값을 어떻게 넘겨받지? 반환 어떻게 ?
+; eax와 ebx 입력값을 ecx에 반환
+MAX:
+    cmp eax, ebx
+    jg L1
+    mov ecx, ebx
+    jmp L2
+L1:
+    mov ecx, eax
+    
+L2:
+    ret
+    
+    ; 그런데 인자가 10개라면 어떻게 할까? a, b, c, d
+    ; eax, ebx에 이미 중요한 값이 있으면 어떻게 할까?
     
     ; 초기화 된 데이터
     ; [변수이름] [크기] [초기값]
