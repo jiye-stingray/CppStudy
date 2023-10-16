dec95bb695ab8752c5d1fe03576ad75cfaeff372
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-10-16 22:43:35 +0900

    🤩 l 함수 기초

--- CPP/CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP/CPP_Study/CPP_Study/CPP_Study.cpp
@@ -3,8 +3,51 @@ using namespace std;
 
 // 오늘의 주제 : 함수 (프로시저, 메소드, 루틴)
 
+/*
+
+	input으로 무엇을 받고, output으로 무엇을 뱉을지 정해준다
+
+	반환타입 함수이름([인자타입 매개변수])
+	{
+		함수 내용
+		return ~~~;
+	}
+
+*/
+
+void PrintHelloWorld()
+{
+	cout << "Hello World" << endl; 
+
+	return;
+}
+
+// 정수를 입력 받아서, 콘솔에 출력하는 함수를 만들어보자
+// input : int // output : 없음
+void PrintNumber(int number)
+{
+	cout << "넘겨주신  숫자는 " << number << " 입니다" << endl;
+}
+
+// 2를 곱하고 결과물을 뱉는 함수
+int MultiplyBy2(int a)
+{
+	return a * 2;
+}
+
+// 두 숫자를 곱해서 결과물을 뱉는 함수를 만들어보자
+
+int MultiplyBy(int a, int b)
+{
+	int c = a * b;
+	return c;
+}
 
 int main()
 {
+	int result = MultiplyBy(3,5);
+	PrintNumber(result);
+	//PrintHelloWorld();
+
 	return 0;
 }
