03b61f4d575e49127f0a8356dcd7323a04c48020
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-10-25 15:01:40 +0900

    🤩 ㅣ 포인터 vs 참조

--- CPP/CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP/CPP_Study/CPP_Study/CPP_Study.cpp
@@ -24,63 +24,97 @@ void CreateMonster(StatInfo info)
 	info.defence = 5;
 }
 
-// 값을 수정하지 않는다면, 양쪽 다 일단 문제 없음
-
-// 1) 값 전달 방식 
-void PrintInfoBycopy(StatInfo info)
+// 특정 조건을 만족하는 몬스터를 찾는 함수
+StatInfo* FindMonster()
 {
-	cout << "--------------------" << endl;
-	cout << "HP: " << info.hp << endl;
-	cout << "ATTCK: " << info.attack << endl;
-	cout << "DEFENCE: " << info.defence << endl;
-	cout << "--------------------" << endl;
+	// TODO : Heap 영역에서 뭔가를 찾아봄 
+	// 찾았다!
+	// return monster~;
 }
 
-// 2) 주소 전달 방식 
-void PrintInfoByPtr(StatInfo* info)
+StatInfo globalInfo;
+
+void PrintInfoByPtr(const StatInfo* const info)
 {
+	if (info == nullptr)
+		return;
+
 	cout << "--------------------" << endl;
 	cout << "HP: " << info->hp << endl;
 	cout << "ATTCK: " << info->attack << endl;
 	cout << "DEFENCE: " << info->defence << endl;
 	cout << "--------------------" << endl;
+
+	// 별 뒤에 붙인다면? 
+	// StatInfo* const info
+	// info 라는 바구니의 내용물(주소)를 바꿀 수 없음
+
+	// 별 앞에 붙인다면 ?
+	// const StatInfo* info
+	// info가 가리키고 있는 바구니의 내용을 바꿀 수 없음
+
+
+	// info[ 주소값 ]		주소값[	데이터	]
+	//info = &globalInfo;
+
+	//info->hp = 10000;
 }
 
-// StatInfo가 1000 바이트짜리 대형 구조체라면 ? 
 
-// 3) 참조 전달 방식
-void PrintInfoByRef(StatInfo& info)
+void PrintInfoByRef(const StatInfo& info)
 {
 	cout << "--------------------" << endl;
 	cout << "HP: " << info.hp << endl;
 	cout << "ATTCK: " << info.attack << endl;
 	cout << "DEFENCE: " << info.defence << endl;
 	cout << "--------------------" << endl;
+
 }
 
+#define OUT
+
+void ChangeInfo(OUT StatInfo& info)
+{
+	info.hp = 100;
+}
 
 int main()
 {
 
-	int number = 1;
+	StatInfo info;
 
-	int* pointer = &number;
-	*pointer = 2;
+	// 포인터 vs 참조
+
+	// 성능 : 똑같음
+	// 편의성 : 참조가 좋다
 
 	// 어셈블리로 보면 포인터랑 똑같음
+	int number = 1;
 	int& reference = number;
 
-	// number라는 바구니에 reference라는 이름을 또 지어주는 것
-	reference = 3;
 
-	// 그런데 귀찮게 또 다른 이름을 짓는 이유는 ?
+	// 2) 초기화 관련 
+	// 참조하는 대상이 없으면 안됨
 
-	StatInfo info;
+	StatInfo* pointer = nullptr;
+
 	CreateMonster(&info);
 
-
-	PrintInfoBycopy(info);
 	PrintInfoByPtr(&info);
 	PrintInfoByRef(info);
+
+	// 그래서 결론은 ? 
+	// Team by Team
+
+	// 명시적으로 호출할 때 OUT을 붙일 수 있음
+	ChangeInfo(OUT info);
+
+
+	// 포인터로 사용하던 걸 참조로 넘겨주면 ?
+	PrintInfoByRef(*pointer);
+
+	// 참조로 사용하던걸 포인터로 사용하면 ?
+	PrintInfoByPtr(pointer);
+
 	return 0;
 }
