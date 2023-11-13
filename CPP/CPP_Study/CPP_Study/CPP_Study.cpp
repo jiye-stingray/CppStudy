9ac0b73ee32a1d4eef27fadf94e8681f16d79774
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-11-13 12:11:04 +0900

    :fire:

--- CPP/CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP/CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,20 +1,10 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 다차원 배열
+// 오늘의 주제
 
 int main()
 {
-	int apart[2][5] = { {4,2,1,3,4},{1,1,5,2,2} };
-
-	for (int floor = 0; floor < 2; floor++)
-	{
-		for (int room = 0; room < 5; room++)
-		{
-			int num = apart[floor][room];
-			cout << num << endl;
-		}
-	}
 
 	return 0;
 }
